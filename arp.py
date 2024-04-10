5c37828e824ac37adc4c0f22c220ffffe1c0b971
David Peters <dave@daveshow.com>
2024-04-10 00:31:01 +0000

    refractor: cleanup

--- arp.py
+++ arp.py
@@ -4,15 +4,13 @@ from scapy.all import send, IP, TCP, Ether, sendp, UDP, DNSQR, DNS, RandShort
 from time import sleep
 from os import environ
 from fcntl import ioctl
-from socket import socket,AF_INET,SOCK_DGRAM
+from socket import socket, AF_INET, SOCK_DGRAM
 from struct import pack
 
 
 def getHwAddr(ifname):
     s = socket(AF_INET, SOCK_DGRAM)
-    info = ioctl(
-        s.fileno(), 0x8927, pack("256s", bytes(ifname, "utf-8")[:15])
-    )
+    info = ioctl(s.fileno(), 0x8927, pack("256s", bytes(ifname, "utf-8")[:15]))
     return ":".join("%02x" % b for b in info[18:24])
 
 
@@ -32,14 +30,26 @@ MX:2\r\n\r\n"""
 print("Container started")
 
 while True:
-    ans = IP(dst=SSDPserver,src=spoofedIPsrc)/UDP(sport=RandShort(), dport=53)/DNS(rd=1,qd=DNSQR(qname="www.google.com",qtype="A"))
+    ans = (
+        IP(dst=SSDPserver, src=spoofedIPsrc)
+        / UDP(sport=RandShort(), dport=53)
+        / DNS(rd=1, qd=DNSQR(qname="www.google.com", qtype="A"))
+    )
     ssdpRequest = (
         Ether(src=fake_mac, dst="ff:ff:ff:ff:ff:ff")
         / IP(src=spoofedIPsrc, dst=SSDPserver)
         / UDP(sport=1900, dport=1900)
         / payload
     )
-    print("Sent from", spoofedIPsrc, "to", SSDPserver, "ethernet address", fake_mac, "sleep 2.5 mins")
+    print(
+        "Sent from",
+        spoofedIPsrc,
+        "to",
+        SSDPserver,
+        "ethernet address",
+        fake_mac,
+        "sleep 2.5 mins",
+    )
     sendp(ans)
     sleep(150)
 
